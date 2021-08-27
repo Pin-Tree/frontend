b0b24bbfb027a24c9a446b9f84295c7bc589c330
somedaycode <somedaycode@gmail.com>
2021-08-27 19:59:33 +0900

    style: [#3] 메인페이지의 각 컴포넌트 구분선 추가

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,23 @@
+import styled from 'styled-components';
+import MainBody from './MainBody';
 import MainHeader from './MainHeader';
 
 function Main() {
-  return <MainHeader />;
+  return (
+    <>
+      <Wrap>
+        <MainHeader />
+      </Wrap>
+      <Wrap>
+        <MainBody />
+      </Wrap>
+    </>
+  );
 }
 
 export default Main;
+
+const Wrap = styled.main`
+  padding: 0.5rem ${({ theme }) => theme.paddings.sidePadding};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.grayPrimary};
+`;
