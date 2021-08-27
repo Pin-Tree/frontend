415e4217581997f472118f17d938efb95df48db9
somedaycode <somedaycode@gmail.com>
2021-08-27 17:46:52 +0900

    feat: [#4] 메인페이지 라우팅

--- App.tsx
+++ App.tsx
@@ -1,5 +1,25 @@
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+import theme from '@src/styles/theme';
+import GlobalStyles from '@src/styles/GlobalStyles';
+import MainPage from '@src/pages/MainPage';
+
 function App() {
-  return <div>나는 앱이다</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <Router>
+          <Switch>
+            <Route path="/" exact>
+              <MainPage />
+            </Route>
+          </Switch>
+        </Router>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
