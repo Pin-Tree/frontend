1b32f00779878e2bc4c32593b6d1114c2f7b2792
somedaycode <somedaycode@gmail.com>
2021-08-31 18:49:06 +0900

    style: 사용하는 테마 스타일 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,4 +1,15 @@
 import { createTheme } from '@material-ui/core';
+import { css } from 'styled-components';
+
+const flexCenter = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const serviceWidth = {
+  default: '1760px',
+};
 
 const paddings = {
   sidePadding: '5rem',
@@ -7,9 +18,13 @@ const paddings = {
 const colors = {
   bg: '#FFFEFA',
   grayPrimary: '#AAAAAA',
+  graySecondary: '#777777',
   grayButton: '#FBFBFB',
+  blackPrimary: '#000000',
   greenPrimary: '#1D8170',
   grayProgressBar: '#222222',
+  centerNode: '#F7F6F0',
+  branchNode: '#F5F4EE',
 };
 
 const fontSizes = {
@@ -20,10 +35,20 @@ const fontSizes = {
   xxl: '36px',
 };
 
+const borderRadius = {
+  s: '12px',
+  m: '14px',
+  base: '16px',
+  l: '18px',
+};
+
 const theme = createTheme({
+  serviceWidth,
   paddings,
   colors,
   fontSizes,
+  borderRadius,
+  flexCenter,
 });
 
 export default theme;
