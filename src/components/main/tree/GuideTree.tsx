722573291cbc136d439d3e3e6a17358d5fe8ff93
somedaycode <somedaycode@gmail.com>
2021-09-06 19:29:32 +0900

    feat: [#9] 메인페이지 트리 가로스크롤 이벤트 적용

--- src/components/main/tree/GuideTree.tsx
+++ src/components/main/tree/GuideTree.tsx
@@ -1,10 +1,30 @@
 import styled from 'styled-components';
+import { useRef, WheelEvent } from 'react';
+
 import Node from './Node';
 
 function GuideTree() {
+  const wheelX = useRef<HTMLDivElement | null>(null);
+
+  const handleMouseWheel = (e: WheelEvent) => {
+    if (!wheelX.current) return;
+    const movement = 500;
+    const isWheelDown = e.deltaY > 0 ? true : false;
+    const currentWheel = wheelX.current;
+
+    if (!isWheelDown) {
+      if (currentWheel.scrollLeft === 0) return;
+      currentWheel.scrollLeft -= movement;
+    }
+
+    if (isWheelDown) {
+      currentWheel.scrollLeft += movement;
+    }
+  };
+
   return (
     <TreeWrap>
-      <TreeContainer>
+      <TreeContainer onWheel={handleMouseWheel} ref={wheelX}>
         <Node />
         <Node />
         <Node />
