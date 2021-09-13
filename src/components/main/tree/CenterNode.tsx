6d466c20906c69e729ae42d46dacae35f892d105
somedaycode <somedaycode@gmail.com>
2021-09-13 18:00:12 +0900

    feat: 노드 위에 마우스를 올렸을 떄 노드 주위에 버튼을 활성화

--- src/components/main/tree/CenterNode.tsx
+++ src/components/main/tree/CenterNode.tsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
 import styled from 'styled-components';
 
+import ActionButtons from '@src/components/common/ActionButtons';
+
 function CenterNode() {
-  return <NodeWrap>이건 센터노드</NodeWrap>;
+  const [isHovering, setIsHovering] = useState(false);
+
+  const handleMouseOver = () => {
+    setIsHovering(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovering(false);
+  };
+
+  return (
+    <NodeWrap onMouseOver={handleMouseOver} onMouseLeave={handleMouseLeave}>
+      이건 센터노드
+      {isHovering && <ActionButtons />}
+    </NodeWrap>
+  );
 }
 
 export default CenterNode;
@@ -15,4 +33,5 @@ const NodeWrap = styled.div`
   border-radius: ${({ theme }) => theme.borderRadius.base};
   top: 50%;
   z-index: 1;
+  position: relative;
 `;
